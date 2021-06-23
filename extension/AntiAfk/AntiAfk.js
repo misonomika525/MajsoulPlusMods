830a142e751cb063e013ef10b205b47f82972579
Tachibana <miho_kohinata@hotmail.com>
2021-06-23 14:00:34 +0800

    12

--- extension/AntiAfk/AntiAfk.js
+++ extension/AntiAfk/AntiAfk.js
@@ -1,8 +1,10 @@
-// 0.2
 // Prevents getting kicked for inactivity
 var waitheatbeat = setInterval(function() {
     if (this && this.GameMgr && this.GameMgr.Inst && Laya) {
-        Laya.timer.loop(1e3, this.GameMgr, this.GameMgr.Inst.clientHeatBeat)
+        setInterval(() => {
+            this.GameMgr.Inst._pre_mouse_point = new Laya.Point(Math.floor(Math.random() * document.getElementById('layaCanvas').width), Math.floor(Math.random() * document.getElementById('layaCanvas').height));
+            document.dispatchEvent(new Event('mousemove'));
+        }, 10000);
         clearInterval(waitheatbeat);
     }
 }, 1000);
