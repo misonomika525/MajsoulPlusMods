c06deabf24583613fad6aefef2a37953c78abea3
TanakaKotoha <tachibana_hana@outlook.com>
2024-03-18 20:45:42 +0800

    wdnmd

--- extension/AntiAfk/AntiAfk.js
+++ extension/AntiAfk/AntiAfk.js
@@ -1,10 +1,8 @@
+// 0.2
 // Prevents getting kicked for inactivity
 var waitheatbeat = setInterval(function() {
     if (this && this.GameMgr && this.GameMgr.Inst && Laya) {
-        setInterval(() => {
-            this.GameMgr.Inst._pre_mouse_point = new Laya.Point(Math.floor(Math.random() * document.getElementById('layaCanvas').width), Math.floor(Math.random() * document.getElementById('layaCanvas').height));
-            document.dispatchEvent(new Event('mousemove'));
-        }, 10000);
+        Laya.timer.loop(1e3, this.GameMgr, this.GameMgr.Inst.clientHeatBeat)
         clearInterval(waitheatbeat);
     }
 }, 1000);
